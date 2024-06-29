1b71c2d7fc60c50e4ec15c2250c9bba1b847aa6c
Hussein <badihussien1988@gmail.com>
2024-06-29 14:14:00 +0300

    Color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,11 @@ function App() {
     
     </GridItem>
     <Show above="lg">  {/* to prevent showing in mobile devices */}
-      <GridItem area='aside' bg='gold'>
+      <GridItem area='aside'>
         Aside
       </GridItem>
     </Show>
-    <GridItem area='main' bg='dodgerblue'>
+    <GridItem area='main'>
       Main
     </GridItem>
   </Grid>
